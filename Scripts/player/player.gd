0260ed8dc923188ca6afef3961fc99a88d997e47
BaldurThor <Baldurthor96@gmail.com>
2025-03-01 12:06:56 +0000

    merge

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -44,16 +44,17 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("aim"):
 		direction = camrot_h.global_transform.basis.z
 
+
+# Added by Baldur #
 @onready var state_machine: AnimationNodeStateMachinePlayback = $Knight/AnimationTree["parameters/playback"]
 
-var instant_jump: bool = true
+var instant_jump: bool = true 
+###################
 
-# Make the velocities seperate for the animation player
-var vertical_velocity: Vector3 = Vector3.ZERO
-var horizontal_velocity: Vector3
 
 func _physics_process(delta: float) -> void:
 	var on_floor = is_on_floor()
+	instant_jump = true # Added by Baldur #
 	
 	if !is_dying:
 		if !on_floor:
@@ -62,6 +63,7 @@ func _physics_process(delta: float) -> void:
 			vertical_velocity = Vector3.DOWN*gravity/10
 		if Input.is_action_just_pressed("jump") and (!is_attacking) and on_floor:
 			vertical_velocity = Vector3.UP * jump_force
+			instant_jump = false # Added by Baldur #
 		angular_acceleration = 10
 		movement_speed = 0
 		acceleration = 15
@@ -90,44 +92,25 @@ func _physics_process(delta: float) -> void:
 			horizontal_velocity = horizontal_velocity.lerp(direction.normalized()*0.01, acceleration * delta)
 		else:
 			horizontal_velocity = horizontal_velocity.lerp(direction.normalized()*movement_speed, acceleration * delta)
+		
+		
+		
+		# Added by Baldur #
+		if not on_floor:
+			jump(instant_jump)
+		elif horizontal_velocity != Vector3.ZERO:
+			run()
+		else:
+			idle()
+		###################
+		
+		
+		
 		velocity.z = horizontal_velocity.z + vertical_velocity.z
 		velocity.x = horizontal_velocity.x + vertical_velocity.x
 		velocity.y = vertical_velocity.y
 		move_and_slide()
 	horizontal_velocity = Vector3.ZERO
-
-	# Handle jump.
-	if is_on_floor():
-		if Input.is_action_just_pressed("ui_accept"):
-			vertical_velocity.y = JUMP_VELOCITY
-			instant_jump = false
-		else:
-			vertical_velocity.y = 0
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		horizontal_velocity.x = direction.x * SPEED
-		horizontal_velocity.z = direction.z * SPEED
-	else:
-		horizontal_velocity.x = move_toward(horizontal_velocity.x, 0, SPEED)
-		horizontal_velocity.z = move_toward(horizontal_velocity.z, 0, SPEED)
-
-	# Add the gravity.
-	if not is_on_floor():
-		vertical_velocity += get_gravity() * delta
-		jump(instant_jump)
-	elif horizontal_velocity == Vector3.ZERO:
-		idle()
-	else:
-		run()
-		
-	# Add the seperate velocities back together
-	velocity = horizontal_velocity + vertical_velocity
-
-	move_and_slide()
 
 func idle() -> void:
 	if state_machine.get_current_node() != "Idle":
